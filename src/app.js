9891f231fd82db469cb9567b36623b7963b2fb89
Pranaysalavadhi <pranaysalavadhi@gmail.com>
2026-03-14 12:44:09 +0530

    Learnt Request handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require("express")
 
 const app = express();
+app.get("/user",(req,res) =>{
+    res.send("User form express js")
+})
 
-app.use((req,res) =>{
+app.get("/",(req,res,next) =>{
     res.send("Hello from backend with nodemon")
+})
+app.get("/random.text",(req,res,next) =>{
+    res.send("random.text")
+})
+
+app.get('/user/:userId/:name/:password',(req,res) =>{
+   console.log(req.params)
+   res.send({firstname: 'Pranay'})
+})
+
+app.all('/secret', (req, res, next) => {
+  console.log('Accessing the secret section ...')
+  next() // pass control to the next handler
 })
 
 app.listen(3000,() =>{
