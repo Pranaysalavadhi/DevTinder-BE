f8168ee1d95c9888322a65db86e38b859b093dfb
Pranaysalavadhi <pranaysalavadhi@gmail.com>
2026-03-19 17:45:27 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -56,11 +56,23 @@ const app = express();
       }
     })
 
-    app.patch('/user', async (req,res) =>{
-      const userId = req.body._id;
+    app.patch('/user/:userId', async (req,res) =>{
+      const userId = req.params?.userId;
       const data = req.body;
 
       try{
+
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+        const isUpdateAllowed = Object.keys(data).every(k =>
+          ALLOWED_UPDATES.includes(k)
+);
+        if(!isUpdateAllowed){
+          throw new Error("Update not Allowed");
+        }
+        if(data?.skills.length > 10){
+          throw new Error(" Skills Cannot be more than 10 ");
+        }
+
         const user = await User.findByIdAndUpdate(userId,data,{
           runValidators: true,
         });
