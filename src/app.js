1ef7e9ed398e4e6b1ade436453f84248c8ff299f
Pranaysalavadhi <pranaysalavadhi@gmail.com>
2026-03-19 11:42:11 +0530

    created CRUD API's

--- src/app.js
+++ src/app.js
@@ -5,17 +5,6 @@ const app = express();
 
  app.use(express.json());
 
- connectDB()
-    .then(() =>{
-        console.log("Database connection established...");
-        app.listen(3000,() =>{
-        console.log(" My app is successfully running on port no 3000") 
-       })
-    })
-    .catch((err) => {
-        console.error("Database cannot be connected!!");
-    })
-
     app.post("/signup", async (req,res) =>{
       // creating a mew instance of the usermodel.
       const user = User(req.body);
@@ -52,4 +41,43 @@ const app = express();
       catch(err){
         res.status(400).send("Something went wrong");
       }
+    })
+
+    app.delete('/user', async (req,res) => {
+      const userId = req.body._id;
+      try{
+        const user = await User.findByIdAndDelete(userId);
+        if(!user)
+          res.status(400).send("User not found");
+
+        res.send("User deleted successfully");
+      }
+      catch(err){
+        res.status(400).send("Something went wrong");
+      }
+    })
+
+    app.patch('/user', async (req,res) =>{
+      const userId = req.body._id;
+      const data = req.body;
+
+      try{
+        const user = await User.findByIdAndUpdate(userId,data);
+        res.send("User updated successfully");
+      }
+      catch(err){
+        res.status(400).send("something went wrong ");
+      }
+
+    })
+
+     connectDB()
+    .then(() =>{
+        console.log("Database connection established...");
+        app.listen(3000,() =>{
+        console.log(" My app is successfully running on port no 3000") 
+       })
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected!!");
     })
